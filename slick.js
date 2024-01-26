7b1a7b16ba8a95e831054cb42d5de14294ee5529
dezoul <aussieme@gmail.com>
2024-01-26 11:59:13 +1100

    mobile clean up

--- slick.js
+++ slick.js
@@ -1,12 +1,42 @@
 window.addEventListener('load', function () {
 
-$('.slider').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
-
-console.log('should have a slider')
+  const sliders = document.querySelectorAll('.gal')
+sliders.forEach(slider => {
+  console.log(slider)
+  $(slider).slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [{
+      breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          arrows: true
+        }
+    }]
+  });
 
 })
+
+if ( this.document.querySelector('.slider') ){
+
+  $('.slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [{
+      breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+
+        }
+    }]
+  })
+
+  }
+  
+  console.log('should have a slider')
+    
+  })
